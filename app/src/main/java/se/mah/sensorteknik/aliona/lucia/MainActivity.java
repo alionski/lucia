692673d2c24c7075b7932d44347d7e07b7b065fa
aliona <alegroma@gmail.com>
2018-03-02 13:26:57 +0100

    Service, controller, and a bit om MainActivity)

--- app/src/main/java/se/mah/sensorteknik/aliona/lucia/MainActivity.java
+++ app/src/main/java/se/mah/sensorteknik/aliona/lucia/MainActivity.java
@@ -1,17 +1,32 @@
 package se.mah.sensorteknik.aliona.lucia;
 
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.le.BluetoothLeScanner;
+import android.bluetooth.le.ScanCallback;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.net.Uri;
+import android.support.annotation.NonNull;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
+
+import java.util.UUID;
 
 public class MainActivity extends AppCompatActivity implements MainFragment.OnFragmentInteractionListener{
+
+    private static final int REQUEST_ENABLE_BT = 5868;
+    private static final int REQUEST_FINE_LOCATION =888;
+    private MainController mController;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mController = new MainController(this);
         initUI();
     }
 
@@ -26,5 +41,35 @@ public class MainActivity extends AppCompatActivity implements MainFragment.OnFr
     @Override
     public void onFragmentInteraction(Uri uri) {
 
+    }
+
+    @Override
+    public void onRequestPermissionsResult(final int requestCode, @NonNull final String[] permissions, @NonNull final int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == REQUEST_FINE_LOCATION) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                if (mController.isBluetoothAdapterEnabled()) {
+                    mController.startScan();
+                }
+            } else {
+                Toast.makeText(this, "You didn't give permission to access device location", Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        // Check which request we're responding to
+        if (requestCode == REQUEST_ENABLE_BT) {
+            if (resultCode == RESULT_OK) {
+                mController.startScan();
+            }
+        }
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mController.disconnect();
     }
 }
